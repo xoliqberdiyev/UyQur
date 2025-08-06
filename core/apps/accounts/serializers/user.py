7e0eb3ec5784b685d1a2a7bd4cd8b37d9fc8e565
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-08-06 12:09:55 +0500

    fix

--- core/apps/accounts/serializers/user.py
+++ core/apps/accounts/serializers/user.py
@@ -22,7 +22,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.username = validated_data.get('first_name', instance.username)
+        instance.username = validated_data.get('username', instance.username)
         instance.profile_image = validated_data.get('profile_image', instance.profile_image)
         instance.save()
         return instance
