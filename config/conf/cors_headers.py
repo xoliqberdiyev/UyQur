f4e572227adbd55ed2500a2d1cb78308da1d2182
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-16 17:12:51 +0500

    fix

--- config/conf/cors_headers.py
+++ config/conf/cors_headers.py
@@ -3,11 +3,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8082",
     "http://127.0.0.1:8081",
     "http://127.0.0.1:8082",
-    'https://uyqur.vercel.app'
+    'https://uyqur.vercel.app',
+    'https://uyqurfront.felixits.uz',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://uyqur.felixits.uz",
     'http://localhost:8001',
     'http://127.0.0.1:8001',
+    'https://test-uyqur.felixits.uz'
 ]
