1705729e1b613f0f4d0fedc1705d59d6804431c7
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-19 16:23:15 +0500

    fix bug

--- core/apps/accounts/admin/role.py
+++ core/apps/accounts/admin/role.py
@@ -7,3 +7,4 @@ from core.apps.accounts.models.role import Role
 class RoleAdmin(admin.ModelAdmin):
     list_display = ['name']
     autocomplete_fields = ("permissions", "permission_to_tabs", 'permission_to_actions')
+    filter_horizontal = ("permissions", "permission_to_tabs", 'permission_to_actions')
