9b5aa5451e794495a8ad5a4037ac743c3b944e6c
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-19 15:35:25 +0500

    fix

--- config/conf/cors_headers.py
+++ config/conf/cors_headers.py
@@ -3,14 +3,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8082",
     "http://127.0.0.1:8081",
     "http://127.0.0.1:8082",
-    'https://uyqur.vercel.app',
     'https://uyqurfront.felixits.uz',
+    "https://ibapp.uz",
+    "https://www.ibapp.uz",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
     'https://test-uyqur.felixits.uz',
-    "https://ibapp.uz",
-    "https://www.ibapp.uz",
+    "https://test-api.ibapp.uz",
 ]
