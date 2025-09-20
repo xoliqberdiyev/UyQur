96ff2973cf8d4e668200cd25d423eae50adb3f44
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-20 14:51:48 +0500

    change

--- core/apps/accounts/views/permission.py
+++ core/apps/accounts/views/permission.py
@@ -2,12 +2,28 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 from core.apps.accounts.serializers import permission as serializers
-from core.apps.accounts.models.permission import Permission
+from core.apps.accounts.models.permission import Permission, PermissionToAction, PermissionToTab
 from core.apps.accounts.permissions.permissions import HasRolePermission
 
 
 class PermissionListApiView(generics.ListAPIView):
-    queryset = Permission.objects.prefetch_related('permission_tab')
     serializer_class = serializers.PermissionListSerializer
+    queryset = None
     permission_classes = [HasRolePermission]
-    pagination_class = None
+
+    def get(self, request):
+        permissions = Permission.objects.all()
+        permission_to_tabs = PermissionToTab.objects.all()
+        permission_to_actions = PermissionToAction.objects.all()
+        serializer = self.serializer_class(permissions, many=True)
+        tab_permissions = serializers.PermissionToTabListSerializer(permission_to_tabs, many=True)
+        action_permissions = serializers.PermissionToActionListSerializer(permission_to_actions, many=True)
+        return Response(
+            {
+                'success': True,
+                'permissions': serializer.data,
+                'tab_permissions': tab_permissions.data,
+                'action_permissions': action_permissions.data
+            },
+            status=200
+        )
