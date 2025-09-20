4c3cb52fc6e667e8914a1f528b8a5769703133da
behruz-dev <xoliqberdiyevbehruz12@gmail.com>
2025-09-20 14:39:11 +0500

    change permissions admin

--- core/apps/accounts/admin/permission.py
+++ core/apps/accounts/admin/permission.py
@@ -6,11 +6,13 @@ from core.apps.accounts.models.permission import Permission, PermissionToTab, Pe
 @admin.register(Permission)
 class PermissionAdmin(admin.ModelAdmin):
     list_display = ['name', 'code']
-
+    filter_horizontal = ['permission_tab']
 
 @admin.register(PermissionToTab)
 class PermissionToTabAdmin(admin.ModelAdmin):
     list_display = ['name', 'code']
+    filter_horizontal = ['permission_to_actions']
+
 
 @admin.register(PermissionToAction)
 class PermissionToActionAdmin(admin.ModelAdmin):
